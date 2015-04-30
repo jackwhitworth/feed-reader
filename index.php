327c3b69cf7aed3ebd4e6c5079771b74e19c61e3
Jack Whitworth <jackwhitworth@Chriss-iMac.local>
2015-04-30 09:33:10 +0100

    Feed list database connection added

--- index.php
+++ index.php
@@ -64,5 +64,8 @@ $(function() {
     });
 });
 </script>
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button>
 </body>
 </html>
